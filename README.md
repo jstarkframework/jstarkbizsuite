d353df818d9b14f3209ab9917b9a33eef2cec24e
PC1 <PC1@PC1-PC>
2019-07-08 17:36:05 +0900

    update

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # jstarkbizsuite
 
-http://jstark.org
-
+http://jstark.org<br />
+<br />
+------------------------------------------------------------------------------------<br />
+<br />
+jstark.application.properties에 DB 설정 이후 초기 데이터는 jstark.init.InitialDataInsert 를 실행하여 DB에 데이터 등록<br />
+<br />
 <br />
 <br />
 vm 옵션 JAVA_OPTS에<br />
@@ -11,6 +15,10 @@ vm 옵션 JAVA_OPTS에<br />
 아래와 같이 옵션을 설정하면<br />
 -Djstark.mail:smtp_host=2.2.2.2<br />
 jstark.mail.properties의 smtp_host 값을 대체한다.<br />
+<br />
+컨테이너 환경의 경우 IP가 유동적이기 때문에 아래와 같이 서비스네임을 지정한다.<br />
+-Djstark.servicename=jstarkbizsuite<br />
+<br />
 <br />
 <br />
 처음 실행시 아래 경로에 암호화 키가 자동 생성되며 배포시 이 키를 함께 배포해야함.<br />
---
+++ src/main/java/jstark/init/InitialDataInsert.java
@@ -0,0 +1,214 @@
+package jstark.init;
+import java.io.File;
+import java.util.ArrayList;
+
+import org.jstark.framework.core.CoreUtils;
+import org.jstark.framework.core.DData;
+import org.jstark.framework.core.JLog;
+import org.jstark.framework.core.SimpleDataBase;
+import org.jstark.framework.core.hs.ApplicationMaster;
+import org.jstark.framework.core.hs.DBMS;
+import org.jstark.platform.core.LangUtils;
+import org.jstark.utils.ExcelUtils;
+
+public class InitialDataInsert
+{
+
+    public static void main(String[] args) throws Exception
+    {
+
+        JLog.info("\n\n\nJSTARK DATA INSERT CHECKING...");
+
+        SimpleDataBase db = new SimpleDataBase();
+        db.query("select count(*) cnt from jstark_user");
+        DData data = db.executeDData();
+
+        if(data.getInt("cnt")==0)
+        {
+            String dbms = CoreUtils.getDBMS();
+
+            ExcelUtils excel=ExcelUtils.getInstance();
+            String[][][] sheet_list=excel.getExcel(ApplicationMaster.getJStarkHome()+File.separator+ApplicationMaster.getJStarkName()+File.separator+"WEB-INF"+File.separator+"resource"+File.separator+"setup"+File.separator+"jstark_database"+File.separator+"jstark_data.xlsx");
+
+            String tablename="";
+            ArrayList column = new ArrayList();
+            ArrayList value = new ArrayList();
+
+            JLog.info("\n\n\nJSTARK DATA INSERT START =>");
+
+            for(int i=0;i<sheet_list.length;i++)
+            {
+                tablename=sheet_list[i][0][0];
+
+                JLog.info(tablename);
+
+                String col="";
+                String col_bind="";
+                String query="";
+
+                column = new ArrayList();
+
+                for(int j=1;j<2;j++)
+                {
+                    for(int k=0;k<sheet_list[i][j].length;k++)
+                    {
+                        column.add(sheet_list[i][j][k]);
+                        if(k==0)
+                        {
+                            col+=""+sheet_list[i][j][k];
+                            col_bind+=":"+sheet_list[i][j][k];
+                        }
+                        else
+                        {
+                            col+=","+sheet_list[i][j][k];
+                            col_bind+=", :"+sheet_list[i][j][k];
+                        }
+                    }
+                }
+
+                for(int j=2;j<sheet_list[i].length;j++)
+                {
+                    value = new ArrayList();
+
+                    for(int k=0;k<sheet_list[i][j].length;k++)
+                    {
+                        String str = sheet_list[i][j][k];
+
+                        value.add(str);
+                    }
+
+                    query=" insert into "+(tablename.toLowerCase())+"("+col+") values("+col_bind+") ";
+
+                    db.query(query);
+                    for(int k=0;k<column.size();k++)
+                    {
+                        if(DBMS.POSTGRESQL.equals(dbms) &&
+                          ("C_SORT".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_MIN".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_SPECIAL".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_NUMBER".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_UPPER".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_LOWER".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_SEQ_NUM".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_SEQ_STR".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_ID".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_SPACE".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_DUP".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_CHANGE".equals(column.get(k).toString().toUpperCase()) ||
+                           "PW_ALOGIN".equals(column.get(k).toString().toUpperCase()) ||
+                           "FAIL_CNT".equals(column.get(k).toString().toUpperCase()) ||
+                           "G_SORT".equals(column.get(k).toString().toUpperCase()) ||
+                           "TIME_OUT".equals(column.get(k).toString().toUpperCase()) ||
+                           "DEPTH1".equals(column.get(k).toString().toUpperCase()) ||
+                           "DEPTH2".equals(column.get(k).toString().toUpperCase()) ||
+                           "DEPTH3".equals(column.get(k).toString().toUpperCase()) ||
+                           "DEPTH4".equals(column.get(k).toString().toUpperCase()) ||
+                           "DEPTH5".equals(column.get(k).toString().toUpperCase()) ||
+                           "POPUP_WIDTH".equals(column.get(k).toString().toUpperCase()) ||
+                           "POPUP_HEIGHT".equals(column.get(k).toString().toUpperCase()) ||
+                           "TR_SORT".equals(column.get(k).toString().toUpperCase()) ||
+                           "MONEY".equals(column.get(k).toString().toUpperCase())
+                            ))
+                        {
+                            db.set(column.get(k).toString(),CoreUtils.toInt(value.get(k)));
+                        }
+                        else
+                        {
+                            db.set(column.get(k).toString(),value.get(k).toString());
+                        }
+                    }
+
+                    //try
+                    //{
+                    db.execute();
+                    //}
+                    //catch(Exception ee)
+                    //{
+                    //}
+                }
+            }
+
+            if(DBMS.ORACLE.equals(dbms))
+            {
+                db.query("update jstark_code set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_code_group set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_lang set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_menu set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_menu_auth set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_menu_group set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_control set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_control_auth set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_security set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_theme set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_tree set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_user set a_date=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS'), LAST_PW_CHANGE=TO_DATE('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+            }
+            else if(DBMS.MSSQL.equals(dbms))
+            {
+                db.query("update jstark_code set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_code_group set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_lang set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_menu set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_menu_auth set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_menu_group set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_control set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_control_auth set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_security set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_theme set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_tree set a_date=convert(datetime,'20070220 13:58:33')");db.execute();
+                db.query("update jstark_user set a_date=convert(datetime,'20070220 13:58:33'), LAST_PW_CHANGE=convert(datetime,'20070220 13:58:33')");db.execute();
+            }
+            else if(DBMS.MARIADB.equals(dbms))
+            {
+                db.query("update jstark_code set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_code_group set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_lang set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_menu set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_menu_auth set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_menu_group set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_control set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_control_auth set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_security set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_theme set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_tree set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+                db.query("update jstark_user set a_date=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T'), LAST_PW_CHANGE=str_to_date(date_format('20070220135833','%Y-%m-%d %T'),'%Y-%m-%d %T')");db.execute();
+            }
+            else if(DBMS.CUBRID.equals(dbms))
+            {
+                db.query("update jstark_code set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_code_group set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_lang set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_menu set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_menu_auth set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_menu_group set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_control set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_control_auth set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_security set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_theme set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_tree set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_user set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS'), LAST_PW_CHANGE=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+            }
+            else if(DBMS.POSTGRESQL.equals(dbms))
+            {
+                db.query("update jstark_code set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_code_group set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_lang set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_menu set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_menu_auth set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_menu_group set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_control set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_control_auth set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_security set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_theme set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_tree set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+                db.query("update jstark_user set a_date=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS'), LAST_PW_CHANGE=TO_TIMESTAMP('2007/02/20 13:58:33','YYYY/MM/DD HH24:MI:SS')");db.execute();
+            }
+
+            LangUtils.init();
+
+            JLog.info("JSTARK DATA INSERT END.\n\n\n");
+        }
+
+        JLog.info("\n\n\nJSTARK DATA INSERT DONE");
+    }
+}
